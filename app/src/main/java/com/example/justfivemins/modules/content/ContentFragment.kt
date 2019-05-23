800bb6dfa2c5b42ac373a2c078618f57f7a081d7
David <davidangulococruera@gmail.com>
2019-05-24 00:34:29 +0200

    working in filter

--- app/src/main/java/com/example/justfivemins/modules/content/ContentFragment.kt
+++ app/src/main/java/com/example/justfivemins/modules/content/ContentFragment.kt
@@ -1,7 +1,6 @@
 package com.example.justfivemins.modules.content
 
 
-import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
@@ -10,13 +9,12 @@ import com.example.justfivemins.R.layout.fragment_content
 import com.example.justfivemins.model.User
 import com.example.justfivemins.modules.base.BaseFragment
 import com.example.justfivemins.modules.base.Configurator
-import com.example.justfivemins.modules.content.content_list_adapter.FeaturedUsersListAdapter
 import kotlinx.android.synthetic.main.fragment_content.*
+import kotlinx.android.synthetic.main.item_filter_people.view.*
 import kotlinx.android.synthetic.main.item_users_card_layout.*
 
 class ContentFragment : BaseFragment() {
     private var users: ArrayList<User> = ArrayList()
-    private lateinit var featuredUsersListAdapter: FeaturedUsersListAdapter
 
 
     override fun onCreateViewId(): Int {
@@ -24,30 +22,19 @@ class ContentFragment : BaseFragment() {
     }
 
     override fun viewCreated(view: View?) {
-        users.addAll(
-            arrayOf(
-                User(name = "Jesus Ortera", description = "18",profileImage = 1),
-                User(name = "Jesus Ortera", description = "18",profileImage = 1),
-                User(name = "Jesus Ortera", description = "18",profileImage = 1),
-                User(name = "Jesus Ortera", description = "18",profileImage = 1),
-                User(name = "Jesus Ortera", description = "18",profileImage = 1),
-                User(name = "Jesus Ortera", description = "18",profileImage = 1),
-                User(name = "Jesus Ortera", description = "18",profileImage = 1)
-                )
-        )
+        filterByCountryContainer.tvFilterName.text = getString(R.string.country)
+        filterByCountryContainer.ivBackgrounnd.setImageResource(R.drawable.country_image)
+        filterByPostalCodeContainer.tvFilterName.text = getString(R.string.postal_code)
+        filterByPostalCodeContainer.ivBackgrounnd.setImageResource(R.drawable.postal_code_image)
+        randomContainer.tvFilterName.text = getString(R.string.random_users)
+        randomContainer.ivBackgrounnd.setImageResource(R.drawable.random_image)
+        filterByCityContainer.tvFilterName.text = getString(R.string.city)
+
         val configurator: Configurator? = null
         setToolbarTitle(getString(R.string.home).toUpperCase())
 
         configurator?.hasToolbar = true
-        initFeaturedList()
 
-    }
-    private fun initFeaturedList() {
-        val layoutManager = LinearLayoutManager(activity,0,false)
-        recyclerViewFeaturedUsersList.layoutManager = layoutManager as RecyclerView.LayoutManager?
-        featuredUsersListAdapter =
-            FeaturedUsersListAdapter(users)
-        recyclerViewFeaturedUsersList.adapter = featuredUsersListAdapter
     }
 
     fun setButtonListener(){
